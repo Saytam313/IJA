0e744fb17bd362d5dbdd608dc9e9f959c77e1ba7
Saytam313 <50132863+Saytam313@users.noreply.github.com>
2019-05-03 00:31:47 +0200

    Init
    First init

---
+++ proj_test/FXML_Test/src/Board_GUI/BoardController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Board_GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Šimon
+ */
+public class BoardController implements Initializable {
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("[1,1]");
+    }
+        @FXML
+    private void field11click(ActionEvent event) {
+        System.out.println("[1,1]");
+    }
+    @FXML
+    private void field12click(ActionEvent event) {
+        System.out.println("[1,2]");
+    }
+    @FXML
+    private void field13click(ActionEvent event) {
+        System.out.println("[1,3]");
+    }
+    @FXML
+    private void field14click(ActionEvent event) {
+        System.out.println("[1,4]");
+    }
+    @FXML
+    private void field15click(ActionEvent event) {
+        System.out.println("[1,5]");
+    }
+    @FXML
+    private void field16click(ActionEvent event) {
+        System.out.println("[1,6]");
+    }
+    @FXML
+    private void field17click(ActionEvent event) {
+        System.out.println("[1,7]");
+    }
+    @FXML
+    private void field18click(ActionEvent event) {
+        System.out.println("[1,8]");
+    }
+    @FXML
+    private void field21click(ActionEvent event) {
+        System.out.println("[2,1]");
+    }
+    @FXML
+    private void field22click(ActionEvent event) {
+        System.out.println("[2,2]");
+    }
+    @FXML
+    private void field31click(ActionEvent event) {
+        System.out.println("[3,1]");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
