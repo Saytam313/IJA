322af6bcdf3d4dffdf8e7cfe2ea072b3e30d18bf
Saytam313 <50132863+Saytam313@users.noreply.github.com>
2019-05-03 18:35:22 +0200

    Rename adresařu a uprava struktury

---
+++ Projekt/src/Board_GUI/BoardController.java
@@ -0,0 +1,317 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Board_GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+/**
+ *
+ * @author Šimon
+ */
+public class BoardController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //tabPanel.getTabs().add(new Tab("Tab 1"));
+    }  
+        
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("[1,1]");
+    }
+    @FXML
+    private void field11click(ActionEvent event) {
+        System.out.println("[1,1]");
+    }
+    @FXML
+    private void field12click(ActionEvent event) {
+        System.out.println("[1,2]");
+    }
+    @FXML
+    private void field13click(ActionEvent event) {
+        System.out.println("[1,3]");
+    }
+    @FXML
+    private void field14click(ActionEvent event) {
+        System.out.println("[1,4]");
+    }
+    @FXML
+    private void field15click(ActionEvent event) {
+        System.out.println("[1,5]");
+    }
+    @FXML
+    private void field16click(ActionEvent event) {
+        System.out.println("[1,6]");
+    }
+    @FXML
+    private void field17click(ActionEvent event) {
+        System.out.println("[1,7]");
+    }
+    @FXML
+    private void field18click(ActionEvent event) {
+        System.out.println("[1,8]");
+    }
+    @FXML
+    private void field21click(ActionEvent event) {
+        System.out.println("[2,1]");
+    }
+    @FXML
+    private void field22click(ActionEvent event) {
+        System.out.println("[2,2]");
+    }
+    @FXML
+    private void field23click(ActionEvent event) {
+        System.out.println("[2,3]");
+    }
+    @FXML
+    private void field24click(ActionEvent event) {
+        System.out.println("[2,4]");
+    }
+    @FXML
+    private void field25click(ActionEvent event) {
+        System.out.println("[2,5]");
+    }
+    @FXML
+    private void field26click(ActionEvent event) {
+        System.out.println("[2,6]");
+    }
+    @FXML
+    private void field27click(ActionEvent event) {
+        System.out.println("[2,7]");
+    }
+    @FXML
+    private void field28click(ActionEvent event) {
+        System.out.println("[2,8]");
+    }
+    @FXML
+    private void field31click(ActionEvent event) {
+        System.out.println("[3,1]");
+    }
+    @FXML
+    private void field32click(ActionEvent event) {
+        System.out.println("[3,2]");
+    }
+    @FXML
+    private void field33click(ActionEvent event) {
+        System.out.println("[3,3]");
+    }
+    @FXML
+    private void field34click(ActionEvent event) {
+        System.out.println("[3,4]");
+    }
+    @FXML
+    private void field35click(ActionEvent event) {
+        System.out.println("[3,5]");
+    }
+    @FXML
+    private void field36click(ActionEvent event) {
+        System.out.println("[3,6]");
+    }
+    @FXML
+    private void field37click(ActionEvent event) {
+        System.out.println("[3,7]");
+    }
+    @FXML
+    private void field38click(ActionEvent event) {
+        System.out.println("[3,8]");
+    }
+    @FXML
+    private void field41click(ActionEvent event) {
+        System.out.println("[4,1]");
+    }
+    @FXML
+    private void field42click(ActionEvent event) {
+        System.out.println("[4,2]");
+    }
+    @FXML
+    private void field43click(ActionEvent event) {
+        System.out.println("[4,3]");
+    }
+    @FXML
+    private void field44click(ActionEvent event) {
+        System.out.println("[4,4]");
+    }
+    @FXML
+    private void field45click(ActionEvent event) {
+        System.out.println("[4,5]");
+    }
+    @FXML
+    private void field46click(ActionEvent event) {
+        System.out.println("[4,6]");
+    }
+    @FXML
+    private void field47click(ActionEvent event) {
+        System.out.println("[4,7]");
+    }
+    @FXML
+    private void field48click(ActionEvent event) {
+        System.out.println("[4,8]");
+    }
+    @FXML
+    private void field51click(ActionEvent event) {
+        System.out.println("[5,1]");
+    }
+    @FXML
+    private void field52click(ActionEvent event) {
+        System.out.println("[5,2]");
+    }
+    @FXML
+    private void field53click(ActionEvent event) {
+        System.out.println("[5,3]");
+    }
+    @FXML
+    private void field54click(ActionEvent event) {
+        System.out.println("[5,4]");
+    }
+    @FXML
+    private void field55click(ActionEvent event) {
+        System.out.println("[5,5]");
+    }
+    @FXML
+    private void field56click(ActionEvent event) {
+        System.out.println("[5,6]");
+    }
+    @FXML
+    private void field57click(ActionEvent event) {
+        System.out.println("[5,7]");
+    }
+    @FXML
+    private void field58click(ActionEvent event) {
+        System.out.println("[5,8]");
+    }
+    @FXML
+    private void field61click(ActionEvent event) {
+        System.out.println("[6,1]");
+    }
+    @FXML
+    private void field62click(ActionEvent event) {
+        System.out.println("[6,2]");
+    }
+    @FXML
+    private void field63click(ActionEvent event) {
+        System.out.println("[6,3]");
+    }
+    @FXML
+    private void field64click(ActionEvent event) {
+        System.out.println("[6,4]");
+    }
+    @FXML
+    private void field65click(ActionEvent event) {
+        System.out.println("[6,5]");
+    }
+    @FXML
+    private void field66click(ActionEvent event) {
+        System.out.println("[6,6]");
+    }
+    @FXML
+    private void field67click(ActionEvent event) {
+        System.out.println("[6,7]");
+    }
+    @FXML
+    private void field68click(ActionEvent event) {
+        System.out.println("[6,8]");
+    }
+    @FXML
+    private void field71click(ActionEvent event) {
+        System.out.println("[7,1]");
+    }
+    @FXML
+    private void field72click(ActionEvent event) {
+        System.out.println("[7,2]");
+    }
+    @FXML
+    private void field73click(ActionEvent event) {
+        System.out.println("[7,3]");
+    }
+    @FXML
+    private void field74click(ActionEvent event) {
+        System.out.println("[7,4]");
+    }
+    @FXML
+    private void field75click(ActionEvent event) {
+        System.out.println("[7,5]");
+    }
+    @FXML
+    private void field76click(ActionEvent event) {
+        System.out.println("[7,6]");
+    }
+    @FXML
+    private void field77click(ActionEvent event) {
+        System.out.println("[7,7]");
+    }
+    @FXML
+    private void field78click(ActionEvent event) {
+        System.out.println("[7,8]");
+    }
+    @FXML
+    private void field81click(ActionEvent event) {
+        System.out.println("[8,1]");
+    }
+    @FXML
+    private void field82click(ActionEvent event) {
+        System.out.println("[8,2]");
+    }
+    @FXML
+    private void field83click(ActionEvent event) {
+        System.out.println("[8,3]");
+    }
+    @FXML
+    private void field84click(ActionEvent event) {
+        System.out.println("[8,4]");
+    }
+    @FXML
+    private void field85click(ActionEvent event) {
+        System.out.println("[8,5]");
+    }
+    @FXML
+    private void field86click(ActionEvent event) {
+        System.out.println("[8,6]");
+    }
+    @FXML
+    private void field87click(ActionEvent event) {
+        System.out.println("[8,7]");
+    }
+    @FXML
+    private void field88click(ActionEvent event) {
+        System.out.println("[8,8]");
+    }
+    @FXML
+    private void addTab(ActionEvent event) {
+        System.out.println("addTab");
+    }
+    @FXML
+    private void resetGame(ActionEvent event) {
+        System.out.println("resetGame");
+    }
+    @FXML
+    private void nahraniZaznamu(ActionEvent event) {
+        System.out.println("nahraniZaznamu");
+    }
+    @FXML
+    private void zpetButton(ActionEvent event) {
+        System.out.println("zpet");
+    }
+    @FXML
+    private void vpredButton(ActionEvent event) {
+        System.out.println("vpred");
+    }
+    @FXML
+    private void stopButton(ActionEvent event) {
+        System.out.println("stop");
+    }
+    @FXML
+    private void playButton(ActionEvent event) {
+        System.out.println("play");
+    }
+    
+}
