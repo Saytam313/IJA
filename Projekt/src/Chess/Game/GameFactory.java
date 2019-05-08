f7b2d82f291c6c9831aa1cff9e434b6d5ab1277e
xtetur01 <48331977+xtetur01@users.noreply.github.com>
2019-05-08 15:30:08 +0200

    Update GameFactory.java

--- Projekt/src/Chess/Game/GameFactory.java
+++ Projekt/src/Chess/Game/GameFactory.java
@@ -18,6 +18,18 @@ public abstract class GameFactory extends java.lang.Object
         Figure vW2 = null;
         Figure vB1 = null;
         Figure vB2 = null;
+        Figure sW1 = null;
+        Figure sW2 = null;
+        Figure sB1 = null;
+        Figure sB2 = null;
+        Figure kW1 = null;
+        Figure kW2 = null;
+        Figure kB1 = null;
+        Figure kB2 = null;
+        Figure krW1 = null;
+        Figure qW1 = null;
+        Figure krB1 = null;
+        Figure qB1 = null;
         Game hra=new GameClass(board);
         
         //nastavi board ze se na nem hraje hra
@@ -32,6 +44,8 @@ public abstract class GameFactory extends java.lang.Object
             f=board.getField(i+1,7);
             hra.create(pB[i],f);
         } 
+        
+        
         //White vez         
         vW1=new Disk(0,board,true); 
         Field f=board.getField(1, 1);
@@ -40,13 +54,79 @@ public abstract class GameFactory extends java.lang.Object
         vW2=new Disk(0,board,true); 
         f=board.getField(8, 1);
         hra.create(vW2,f);
+                
         //Black vez
         vB1=new Disk(0,board,false); 
         f=board.getField(1, 8);
         hra.create(vB1,f);
+        
         vB2=new Disk(0,board,false); 
         f=board.getField(8, 8);
         hra.create(vB2,f);
+        
+        
+        
+        //White strelec         
+        sW1=new Disk(2,board,true); 
+        Field f=board.getField(3, 1);
+        hra.create(sW1,f);
+        
+        sW2=new Disk(2,board,true); 
+        f=board.getField(6, 1);
+        hra.create(sW2,f);
+        
+        //Black strelec
+        sB1=new Disk(2,board,false); 
+        f=board.getField(3, 8);
+        hra.create(sB1,f);
+        
+        sB2=new Disk(2,board,false); 
+        f=board.getField(6, 8);
+        hra.create(sB2,f);
+        
+        
+        
+        //White Kun         
+        kW1=new Disk(3,board,true); 
+        Field f=board.getField(2, 1);
+        hra.create(kW1,f);
+        
+        kW2=new Disk(3,board,true); 
+        f=board.getField(7, 1);
+        hra.create(kW2,f);
+        
+        //Black Kun
+        kB1=new Disk(3,board,false); 
+        f=board.getField(2, 8);
+        hra.create(kB1,f);
+        
+        kB2=new Disk(3,board,false); 
+        f=board.getField(7, 8);
+        hra.create(kB2,f);
+        
+        
+        
+        //White Kralovna         
+        qW1=new Disk(4,board,true); 
+        Field f=board.getField(4, 1);
+        hra.create(qW1,f);
+        
+        //Black Kralovna
+        qB1=new Disk(4,board,false); 
+        f=board.getField(4, 8);
+        hra.create(qB1,f);
+        
+        
+        
+        //White Kral         
+        krW1=new Disk(5,board,true); 
+        Field f=board.getField(5, 1);
+        hra.create(krW1,f);
+        
+        //Black Kral
+        krB1=new Disk(5,board,false); 
+        f=board.getField(5, 8);
+        hra.create(krB1,f);
 
         return hra;
     
