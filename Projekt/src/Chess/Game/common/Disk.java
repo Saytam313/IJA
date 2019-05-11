ad6b5ccf801ea848cb79ad4d8d8f5b6a1f9e5ff8
xtetur01 <48331977+xtetur01@users.noreply.github.com>
2019-05-11 18:41:55 +0200

    Kompletni pohyby figurek

--- Projekt/src/Chess/Game/common/Disk.java
+++ Projekt/src/Chess/Game/common/Disk.java
@@ -117,6 +117,51 @@ public class Disk extends java.lang.Object implements Figure{
         return true;
     }
     
+    private int moveKR(Field next, Field moveTo, int ok) {
+        if(next.isEmpty()) {
+            ok++;
+        } else if(((next.get().getType() != 1) && (next.get().getType() != 4)) || (next == moveTo) || (next.get().isWhite() == this.isWhite)) {
+            ok++;
+        }
+        return ok;
+    }
+    
+    private int moveKD(Field next, Field moveTo, int ok) {
+        if(next.isEmpty()) {
+            ok++;
+        } else if(((next.get().getType() != 2) && (next.get().getType() != 4)) || (next == moveTo) || (next.get().isWhite() == this.isWhite)) {
+            ok++;
+        }
+        return ok;
+    }
+    
+    private int moveKP(Field next, Field moveTo, int ok) {
+        if(next.isEmpty()) {
+            ok++;
+        } else if(((next.get().getType() != 0)) || (next == moveTo) || (next.get().isWhite() == this.isWhite)) {
+            ok++;
+        }
+        return ok;
+    }
+    
+    private int moveKJ(Field next, Field moveTo, int ok) {
+        if(next.isEmpty()) {
+            ok++;
+        } else if(((next.get().getType() != 3)) || (next == moveTo) || (next.get().isWhite() == this.isWhite)) {
+            ok++;
+        }
+        return ok;
+    }
+    
+    private int moveKK(Field next, Field moveTo, int ok) {
+        if(next.isEmpty()) {
+            ok++;
+        } else if(((next.get().getType() != 5)) || (next == moveTo) || (next.get().isWhite() == this.isWhite)) {
+            ok++;
+        }
+        return ok;
+    }
+    
     @Override
     public boolean move(Field moveTo){
         
@@ -404,7 +449,341 @@ public class Disk extends java.lang.Object implements Figure{
             }
         } else if(this.typ == 5) {
             if((Math.abs(this.field.getCol() - moveTo.getCol()) <= 1) && (Math.abs(this.field.getRow() - moveTo.getRow()) <= 1)) {
-                return this.moveLong(moveTo);
+                Field next = moveTo;
+                int endU = 8 - moveTo.getRow();
+                int ok = 0;
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.U);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKR(next, moveTo, ok);
+                
+                next = moveTo;
+                endU = moveTo.getRow() - 1;
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.D);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKR(next, moveTo, ok);
+                
+                next = moveTo;
+                endU = moveTo.getCol() - 1;
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.L);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKR(next, moveTo, ok);
+                
+                next = moveTo;
+                endU = 8 - moveTo.getCol();
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.R);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKR(next, moveTo, ok);
+                
+                next = moveTo;
+                if((moveTo.getCol() - 1) >= (8 - moveTo.getRow())) {
+                    endU = 8 - moveTo.getRow();
+                } else {
+                    endU = moveTo.getCol() - 1;
+                }
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.LU);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKD(next, moveTo, ok);
+                
+                next = moveTo;
+                if((moveTo.getCol() - 1) >= (moveTo.getRow() - 1)) {
+                    endU = moveTo.getRow() - 1;
+                } else {
+                    endU = moveTo.getCol() - 1;
+                }
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.LD);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKD(next, moveTo, ok);
+                
+                next = moveTo;
+                if((8 - moveTo.getCol()) >= (8 - moveTo.getRow())) {
+                    endU = 8 - moveTo.getRow();
+                } else {
+                    endU = 8 - moveTo.getCol();
+                }
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.RU);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKD(next, moveTo, ok);
+                
+                next = moveTo;
+                if((8 - moveTo.getCol()) >= (moveTo.getRow() - 1)) {
+                    endU = moveTo.getRow() - 1;
+                } else {
+                    endU = 8 - moveTo.getCol();
+                }
+                while(endU > 0) {
+                    if(next.isEmpty()) {
+                        next = next.nextField(Field.Direction.RD);
+                    } else {
+                        break;
+                    }
+                    endU--;
+                }
+                ok = this.moveKD(next, moveTo, ok);
+                
+                if(this.isWhite) {
+                    if((moveTo.getCol() > 1) && (moveTo.getRow() < 8)) {
+                        next = moveTo.nextField(Field.Direction.LU);
+                        ok = this.moveKP(next, moveTo, ok);
+                    } else {
+                        ok++;
+                    }
+                        
+                    if((moveTo.getCol() < 8) && (moveTo.getRow() < 8)){
+                        next = moveTo.nextField(Field.Direction.RU);
+                        ok = this.moveKP(next, moveTo, ok);
+                    } else {
+                        ok++;
+                    }
+                } else {
+                    if((moveTo.getCol() > 1) && (moveTo.getRow() > 1)) {
+                        next = moveTo.nextField(Field.Direction.LD);
+                        ok = this.moveKP(next, moveTo, ok);
+                    } else {
+                        ok++;
+                    }
+                    
+                    if((moveTo.getCol() < 8) && (moveTo.getRow() > 1)) {
+                        next = moveTo.nextField(Field.Direction.RD);
+                        ok = this.moveKP(next, moveTo, ok);
+                    } else {
+                        ok++;
+                    }
+                }
+                
+                if((moveTo.getCol() > 2) && (moveTo.getRow() < 8)) {
+                    next = moveTo.nextField(Field.Direction.U).nextField(Field.Direction.L).nextField(Field.Direction.L);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() > 2) && (moveTo.getRow() > 1)) {
+                    next = moveTo.nextField(Field.Direction.D).nextField(Field.Direction.L).nextField(Field.Direction.L);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() < 7) && (moveTo.getRow() < 8)) {
+                    next = moveTo.nextField(Field.Direction.U).nextField(Field.Direction.R).nextField(Field.Direction.R);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() < 7) && (moveTo.getRow() > 1)) {
+                    next = moveTo.nextField(Field.Direction.D).nextField(Field.Direction.R).nextField(Field.Direction.R);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() > 1) && (moveTo.getRow() < 7)) {
+                    next = moveTo.nextField(Field.Direction.U).nextField(Field.Direction.U).nextField(Field.Direction.L);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() > 1) && (moveTo.getRow() > 2)) {
+                    next = moveTo.nextField(Field.Direction.D).nextField(Field.Direction.D).nextField(Field.Direction.L);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() < 8) && (moveTo.getRow() < 7)) {
+                    next = moveTo.nextField(Field.Direction.U).nextField(Field.Direction.U).nextField(Field.Direction.R);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() < 8) && (moveTo.getRow() > 2)) {
+                    next = moveTo.nextField(Field.Direction.D).nextField(Field.Direction.D).nextField(Field.Direction.R);
+                    ok = this.moveKJ(next, moveTo, ok);
+                } else {
+                    ok++;
+                }
+                
+                if((moveTo.getCol() == 1) && (moveTo.getRow() == 8)) {
+                    ok += 5;
+                    next = moveTo.nextField(Field.Direction.R);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.RD);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if((moveTo.getCol() == 8) && (moveTo.getRow() == 8)) {
+                    ok += 5;
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.LD);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if((moveTo.getCol() == 1) && (moveTo.getRow() == 1)) {
+                    ok += 5;
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.RU);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.R); 
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if((moveTo.getCol() == 8) && (moveTo.getRow() == 1)) {
+                    ok += 5;
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.LU);
+                    ok = this.moveKK(next, moveTo, ok);
+
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if(moveTo.getCol() == 1) {
+                    ok += 3;
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.R);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if(moveTo.getCol() == 8) {
+                    ok += 3;
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.LD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.LU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if(moveTo.getRow() == 8) {
+                    ok += 3;
+                    next = moveTo.nextField(Field.Direction.R);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.LD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else if(moveTo.getRow() == 1) {
+                    ok += 3;
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.LU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.R);
+                    ok = this.moveKK(next, moveTo, ok);
+                } else {
+                    next = moveTo.nextField(Field.Direction.LU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.U);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RU);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.R);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.RD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.D);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.LD);
+                    ok = this.moveKK(next, moveTo, ok);
+                    
+                    next = moveTo.nextField(Field.Direction.L);
+                    ok = this.moveKK(next, moveTo, ok);
+                }
+                
+                if(ok == 26) {
+                    return this.moveLong(moveTo);
+                } else {
+                    return false;
+                }
+                
             } else {
                 return false;
             }
